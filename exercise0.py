f2db3bd00487911fb272b3f37df2e2091aedba1f
chellshelove <chellshelove733@gmail.com>
2024-10-02 10:16:45 +0900

    done exercise0

---
+++ exercise0.py
@@ -0,0 +1,9 @@
+# Get input strings from the user
+first_string = input("Enter the first string: ")
+second_string = input("Enter the second string: ")
+
+# Find the index of the second string in the first string
+index = first_string.find(second_string)
+
+# Print the result
+print(index)
